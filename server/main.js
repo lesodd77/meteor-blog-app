0d45b1a7e3af7f269cc4492cb647027c79dc5dab
Kelly Copley <copleykj@gmail.com>
2022-10-12 11:21:43 -0400

    get cloudinary working

--- server/main.js
+++ server/main.js
@@ -9,31 +9,32 @@ import '../app/api/posts.publication';
 import '../app/api/ContactsCollection';
 import '../app/api/posts.collection';
 
-
+const settings = Meteor.settings;
 Cloudinary.config({
-    cloud_name: 'cloud_name',
-    api_key: 'api_key',
-    api_secret: 'api_secret',
-  });
-  
+  cloud_name: settings.public.cloudinary_cloud_name,
+  api_key: settings.public.cloudinary_api_key,
+  api_secret: settings.cloudinary_api_secret,
+});
+
   // Rules are bound to the connection from which they are are executed. This means you have a userId available as this.userId if there is a logged in user. Throw a new Meteor.Error to stop the method from executing and propagate the error to the client. If rule is not set a standard error will be thrown.
   Cloudinary.rules.delete = function (publicId) {
     if (!this.userId && !publicId) throw new Meteor.Error("Not Authorized", "Sorry, you can't do that!");
   };
-  
-  Cloudinary.rules.sign_upload = function () {
-    if (!this.userId) throw new Meteor.Error("Not Authorized", "Sorry, you can't do that!")
+
+Cloudinary.rules.sign_upload = function () {
+  return true;
+    // if (!this.userId) throw new Meteor.Error("Not Authorized", "Sorry, you can't do that!")
   };
-  
+
   Cloudinary.rules.private_resource = function (publicId) {
     if (!this.userId && !publicId) throw new Meteor.Error("Not Authorized", "Sorry, you can't do that!");
   };
-  
+
   Cloudinary.rules.download_url = function (publicId) {
     if (!this.userId && !publicId) throw new Meteor.Error("Not Authorized", "Sorry, you can't do that!");
   };
-  
-  
+
+
 Meteor.startup(() => {
-  
+
 });
